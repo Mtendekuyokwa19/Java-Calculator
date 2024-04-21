b0e11a76adc77c3872d3460c49a29fa9beeda386
mtende kuyokwa <mtendekuyokwa19@gmail.com>
2024-04-21 11:20:28 +0200

    update:development of the UI

---
+++ src/main/java/UI/contacteditor/ContactEditor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package UI.contacteditor;
+
+/**
+ *
+ * @author mtende
+ */
+public class ContactEditor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
